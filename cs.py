5463b49403f9c18d39fc1f4807bb056b31786864
rachelemantero <163310457+rachelemantero@users.noreply.github.com>
2024-03-24 17:19:16 +0100

    g

--- cs.py
+++ cs.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from feature_01.py import return_even
+
 st.write('yah')
 
 list_1= [i for i in range(10)]
-even_list=return_even(list_1)
-st.write (even_list)
+
+st.write (list_1)
