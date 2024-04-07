e5edd0cf518241ee7d3eabb3e608dbc1b7adc208
rachelemantero <163310457+rachelemantero@users.noreply.github.com>
2024-04-07 20:35:32 +0200

    s

--- cs.py
+++ cs.py
@@ -1,29 +1,25 @@
 import streamlit as st
 from two_states import run_visa_checker
 
-# Custom CSS to inject into Streamlit
-custom_css = """
-<style>
-    .reportview-container .main .block-container {
-        padding-top: 5rem;
-        padding-bottom: 5rem;
-    }
-    .reportview-container .main {
-        background-image: url('https://www.synergytravelsindia.com/wp-content/uploads/2020/08/schengen-travel-visa.jpg');
-        background-size: cover;
-    }
-    h1 {
-        color: #f63366;
-    }
-    .sidebar .sidebar-content {
-        background-color: #f1f1f1;
-    }
-</style>
-"""
 
-st.markdown(custom_css, unsafe_allow_html=True)
+def set_background_image():
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("https://your-image-url.com/your-image.jpg");
+            background-size: cover;
+            background-position: center center;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 
-st.title('🌍 Check-it')
+# Call the function to set the background image
+set_background_image()
+
+st.title('Check-it')
 
 app_mode = st.sidebar.radio(
     "Choose the feature you want to use:",
