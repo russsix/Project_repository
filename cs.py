52ca23b7742bb0f6748069f77d8e540b2145f0d6
rachelemantero <163310457+rachelemantero@users.noreply.github.com>
2024-04-07 20:23:03 +0200

    s

--- cs.py
+++ cs.py
@@ -1,22 +1,12 @@
 # cs.py
 from two_states import run_visa_checker
+st.title('Check-it')
+app_mode = st.sidebar.radio(
+    "Choose the feature you want to use:",
+    ("Visa Requirement Checker", "Visa-Free Destinations", "Flight Suggestions")
+)
+if app_mode == "Visa Requirement Checker":
+    run_visa_checker()
 
-# Run the visa checker feature imported from two_states.py
-run_visa_checker()
 
 
-# Custom CSS
-st.markdown("""
-<style>
-.custom-style {
-    background-color: #262730;
-    color: #21a1f1;
-    padding: 10px;
-    border-radius: 10px;
-}
-</style>
-""", unsafe_allow_html=True)
-
-# Using the custom style in a container
-with st.container():
-    st.markdown('<div class="custom-style">This is a custom-styled container</div>', unsafe_allow_html=True)
