e0b038e7ce22f14572618105f7d354687631e677
rachelemantero <163310457+rachelemantero@users.noreply.github.com>
2024-04-07 20:25:22 +0200

    s

--- cs.py
+++ cs.py
@@ -1,15 +1,18 @@
-# cs.py
 from two_states import run_visa_checker
+
 st.title('Check-it')
+
 app_mode = st.sidebar.radio(
     "Choose the feature you want to use:",
     ("Visa Requirement Checker", "Visa-Free Destinations", "Flight Suggestions")
 )
+
 if app_mode == "Visa Requirement Checker":
     run_visa_checker()
-else:
-    None
-
+elif app_mode == "Visa-Free Destinations":
+    st.info("The Visa-Free Destinations feature is coming soon.")
+elif app_mode == "Flight Suggestions":
+    st.info("The Flight Suggestions feature is coming soon.")
 
 
 
