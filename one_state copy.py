732f51106c5ccfafe8c066217a136b1034d50f35
rachelemantero <163310457+rachelemantero@users.noreply.github.com>
2024-04-23 22:13:40 +0200

    s

---
+++ one_state copy.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import requests
+
+
+def get_visa_free_destinations(passport_country):
+    api_url = f'https://rough-sun-2523.fly.dev/api/{passport_country}'
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        result = response.json()
+        return result  
+    return []
+   
+def main():
+    st.title("Visa-Free Travel Destinations")
+    passport_country = st.text_input("Enter your passport country: ")
+    if st.button("Search"):
+        visa_free_destinations = get_visa_free_destinations(passport_country)
+        if visa_free_destinations:
+            st.write(f"Visa-Free travel destinations for {passport_country}:")
+            for destination_country in visa_free_destinations:
+                st.write(destination_country)
+        else:
+            st.write("No visa-free travel destinations found.")
+
+if __name__ == "__main__":
+    main()
+
