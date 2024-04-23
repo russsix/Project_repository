f693cc8272906218a6f735ec515730a94dad6ee7
rachelemantero <163310457+rachelemantero@users.noreply.github.com>
2024-04-23 22:57:06 +0200

    s

--- one_state_copy.py
+++ one_state_copy.py
@@ -12,7 +12,7 @@ def run_visa_checker():
     if departure_country and not departure_code:
         st.error(f"'{departure_country}' is not recognized. Please enter a valid country name.")
 
-    if st.button('Visa Country Status') and departure_code and destination_code:
+    if st.button('Visa Country Status') and departure_code:
         url = f'https://rough-sun-2523.fly.dev/api/{departure_code}'
         response = requests.get(url)
         if response.status_code == 200:
