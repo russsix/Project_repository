d13f8fd71f08776be2182b02a237630a5aee4182
rachelemantero <163310457+rachelemantero@users.noreply.github.com>
2024-04-23 23:46:27 +0200

    s

--- DataBase_Countries.py
+++ DataBase_Countries.py
@@ -261,5 +261,3 @@ country_names = {code: country for country, code in country_codes.items()}
 def get_country_name(code):
     normalized_code = code.strip().upper()
     return country_names.get(normalized_code, None)
-
-st.write ('hi')
