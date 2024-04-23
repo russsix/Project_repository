764aa01d46eec5b550d9e196ee0dfb0cf0bfb670
zeliasara <164767492+zeliasara@users.noreply.github.com>
2024-04-23 19:45:16 +0200

    Update one_state.py

--- one_state.py
+++ one_state.py
@@ -2,14 +2,10 @@ import streamlit as st
 import requests
 from DataBase_Countries import get_country_code
 
-
-
 def destinations(passport_country):
     api_url = f'https://rough-sun-2523.fly.dev/api/{passport_country}'
     response = requests.get(api_url)
     
-   
-
 def main():
     st.title("Visa-Free Travel Destinations")
     passport_country = st.text_input("Enter your passport country: ")
@@ -22,5 +18,3 @@ def main():
         else:
             st.write("No visa-free travel destinations found.")
 
-if __name__ == "__main__":
-    main()
