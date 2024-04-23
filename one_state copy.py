a04fab264fae854c9e791ca3c651f04f2e15fa65
rachelemantero <163310457+rachelemantero@users.noreply.github.com>
2024-04-23 22:52:39 +0200

    s

--- one_state copy.py
+++ one_state copy.py
@@ -1,30 +1,23 @@
 import streamlit as st
 import requests
+from DataBase_Countries import get_country_code
 
-def get_visa_free_destinations(passport_country):
-    api_url = f'https://rough-sun-2523.fly.dev/api/{passport_country}'
-    response = requests.get(api_url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None  # or return an empty list depending on what you expect
+def run_visa_checker():
+    st.title('Visa Country Status')
 
-def main():
-    st.title("Visa-Free Travel Destinations")
-    passport_country = st.text_input("Enter your passport country: ")
-    if st.button("Search"):
-        visa_info = get_visa_free_destinations(passport_country)
-        if visa_info:
-            st.write(f"Visa-Free travel destinations for {passport_country}:")
-            st.write("Visa Required:", ', '.join(visa_info['vr']['data']))
-            st.write("Visa on Arrival:", ', '.join(visa_info['voa']['data']))
-            st.write("Visa Free:", ', '.join(visa_info['vf']['data']))
-            if visa_info['cb']['data']:  # Check if list is not empty
-                st.write("Covid Ban:", ', '.join(visa_info['cb']['data']))
-            if visa_info['na']['data']:  # Check if list is not empty
-                st.write("No Admission:", ', '.join(visa_info['na']['data']))
+    departure_country = st.text_input("Enter your departure country:", key='departure_country')
+
+    departure_code = get_country_code(departure_country) if departure_country else None
+    
+    if departure_country and not departure_code:
+        st.error(f"'{departure_country}' is not recognized. Please enter a valid country name.")
+
+    if st.button('Visa Country Status') and departure_code and destination_code:
+        url = f'https://rough-sun-2523.fly.dev/api/{departure_code}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
         else:
-            st.write("No visa information found or failed to retrieve data.")
+            st.error(f"Failed to retrieve data. Status code: {response.status_code}")
+run_visa_checker()
 
-if __name__ == "__main__":
-    main()
