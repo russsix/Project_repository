b7021be429191435bc33b94c620d418038b78b34
rachelemantero <163310457+rachelemantero@users.noreply.github.com>
2024-04-23 19:13:02 +0200

    s

--- two_states.py
+++ two_states.py
@@ -8,27 +8,34 @@ def run_visa_checker():
     departure_country = st.text_input("Enter your departure country:", key='departure_country')
     destination_country = st.text_input("Enter your destination country:", key='destination_country')
 
-    
     departure_code = get_country_code(departure_country) if departure_country else None
     destination_code = get_country_code(destination_country) if destination_country else None
 
-    
     if departure_country and not departure_code:
         st.error(f"'{departure_country}' is not recognized. Please enter a valid country name.")
     if destination_country and not destination_code:
         st.error(f"'{destination_country}' is not recognized. Please enter a valid country name.")
 
-    
     if st.button('Check Visa Requirement') and departure_code and destination_code:
         url = f'https://rough-sun-2523.fly.dev/api/{departure_code}/{destination_code}'
         response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
-            visa_required = 'visa required' in data.get('category', '').lower()
-            if visa_required:
-                st.success('A visa is required.')
+            category = data.get('category', '').lower()
+            
+            if 'visa required' in category:
+                st.error('A visa is required.')
+            elif 'visa on arrival' in category:
+                st.warning('You need to obtain a visa upon arrival.')
+            elif 'visa free' in category:
+                st.success('You can enter the country without a visa.')
+            elif 'covid ban' in category:
+                st.warning('Travel is currently banned due to Covid-19 restrictions.')
+            elif 'no admission' in category:
+                st.error('No entry is permitted to travelers from your country.')
             else:
-                st.info('A visa is not required.')
+                st.info('The visa requirement for your destination is not clear or is unspecified.')
         else:
             st.error(f"Failed to retrieve data. Status code: {response.status_code}")
+
 run_visa_checker()
