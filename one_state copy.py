aea74a82d9991695e392430c77e618dac6a8999c
rachelemantero <163310457+rachelemantero@users.noreply.github.com>
2024-04-23 22:22:03 +0200

    s

--- one_state copy.py
+++ one_state copy.py
@@ -5,10 +5,7 @@ import requests
 def get_visa_free_destinations(passport_country):
     api_url = f'https://rough-sun-2523.fly.dev/api/{passport_country}'
     response = requests.get(api_url)
-    if response.status_code == 200:
-        result = response.json()
-        return result  
-    return []
+    return response.json()
    
 def main():
     st.title("Visa-Free Travel Destinations")
