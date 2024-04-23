962bbc55f2d2e0471413922fac41ec40f337fc89
rachelemantero <163310457+rachelemantero@users.noreply.github.com>
2024-04-23 23:39:56 +0200

    s

--- DataBase_Countries.py
+++ DataBase_Countries.py
@@ -263,3 +263,6 @@ def get_country_name(code):
     return country_names.get(normalized_code, "Unknown Country")
 
 st.write ('hi')
+test_code = 'CH'  # Assuming this is a code you received from the API
+country_name = get_country_name(test_code)
+st.write(assert country_name == 'Switzerland')
