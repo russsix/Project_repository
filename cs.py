642f67b01ebe106d36c94c8507dcea4307bea9ea
rachelemantero <163310457+rachelemantero@users.noreply.github.com>
2024-04-24 15:03:25 +0200

    s

--- cs.py
+++ cs.py
@@ -1,3 +1,4 @@
+#cs.py
 import streamlit as st
 from two_states import run_visa_checker
 from one_state_copy import run_visa_country_status
@@ -8,11 +9,11 @@ def run_flight_comparison():
 
 st.sidebar.title('Navigation')
 app_option = st.sidebar.selectbox('Choose a feature:',
-    ('Two States', 'Visa-Free Destinations', 'Flight Comparison'))
+    ('Two States', 'Visa Country Status', 'Flight Comparison'))
 
 if app_option == 'Two States':
     run_visa_checker()
-elif app_option == 'Visa-Free Destinations':
+elif app_option == 'Visa Country Status':
     run_visa_country_status()
 elif app_option == 'Flight Comparison':
     run_flight_comparison()
