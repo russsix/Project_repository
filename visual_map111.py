0e89cfb0ceea602616bafa1010f5659ad8411fbd
zeliasara <164767492+zeliasara@users.noreply.github.com>
2024-04-24 16:32:51 +0200

    Create visual_map111.py

---
+++ visual_map111.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import requests
+import geopandas as gpd
+import matplotlib.pyplot as plt
+from DataBase_Countries import get_country_code, get_country_name
+
+def get_country_color(country_code, visa_status):
+    if visa_status == 'vf':
+        return 'green'  # Visa Free
+    elif visa_status == 'voa':
+        return 'orange'  # Visa on Arrival
+    elif visa_status == 'vr':
+        return 'red'  # Visa Required
+    else:
+        return 'gray'  # No data
+
+def plot_map(visa_data):
+    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+
+    for index, row in world.iterrows():
+        country_code = get_country_code(row['name'])
+        if country_code in visa_data:
+            visa_status = visa_data[country_code]
+            world.loc[index, 'color'] = get_country_color(country_code, visa_status)
+        else:
+            world.loc[index, 'color'] = 'gray'
+
+    fig, ax = plt.subplots(figsize=(15, 10))
+    world.plot(ax=ax, color=world['color'])
+    ax.set_title('Visa Requirements Map')
+    ax.set_axis_off()
+    st.pyplot(fig)
+
+def run_visa_country_status():
+    st.title('Visa Country Status')
+    passport_country = st.text_input("Enter your departure country:", key='departure_country')
+    passport_code = get_country_code(passport_country) if passport_country else None
+    
+    if passport_country and not passport_code:
+        st.error(f"'{passport_country}' is not recognized. Please enter a valid country name.")
+
+    if st.button('Visa Country Status') and passport_code:
+        url = f'https://rough-sun-2523.fly.dev/api/{passport_code}'
+        response = requests.get(url)
+        data = response.json()
+        if data:
+            visa_data = {}
+            visa_required_countries = data.get('vr', {}).get('data', [])
+            visa_on_arrival_countries = data.get('voa', {}).get('data', [])
+            visa_free_countries = data.get('vf', {}).get('data', [])
+            for country_code in visa_required_countries:
+                visa_data[country_code] = 'vr'
+            for country_code in visa_on_arrival_countries:
+                visa_data[country_code] = 'voa'
+            for country_code in visa_free_countries:
+                visa_data[country_code] = 'vf'
+
+            plot_map(visa_data)
+
+run_visa_country_status()
