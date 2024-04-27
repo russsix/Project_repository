978739b6cf12129942c5af557e815993c7b605df
rachelemantero <163310457+rachelemantero@users.noreply.github.com>
2024-04-28 00:28:06 +0200

    s

--- visual_map_rachele.py
+++ visual_map_rachele.py
@@ -1,31 +1,45 @@
 import streamlit as st
 import pydeck as pdk
+import geopandas as gpd
+import pandas as pd
 
-# Define the GeoJsonLayer
+# Load geographical data from the GeoJSON file
+geo_data = gpd.read_file(r"D:\Download\global_states.geojson")
+
+# Initialize a column for colors in hexadecimal, with a default value
+default_color = [34, 139, 34, 160]  # Dark green with some transparency
+us_color = [255, 0, 0, 160]  # Red with some transparency
+
+# We'll create a new column in the GeoDataFrame for the color
+geo_data['color'] = geo_data.apply(
+    lambda x: us_color if x['ADMIN'] == 'United States of America' else default_color, axis=1
+)
+
+# Convert GeoDataFrame to JSON
+geojson = geo_data.__geo_interface__
+
+# Define the Pydeck layer as a GeoJsonLayer
 layer = pdk.Layer(
     'GeoJsonLayer',
-    'D:/Download/global_states.geojson',
-    get_fill_color='[200, 30, 0, 160]',  # Set the fill color to a semi-transparent red
-    get_line_color='[255, 255, 255, 255]',  # Set the line color to white for the boundaries
-    pickable=True,
-    auto_highlight=True,
+    geojson,
+    stroked=False,
+    filled=True,
+    extruded=True,
+    get_fill_color='color',  # Use 'color' column for the fill color
 )
 
-# Define the initial view state with a wider longitude span
+# Set the initial view state
 view_state = pdk.ViewState(
-    longitude=0,
-    latitude=0,
-    zoom=1.1,  # The zoom level that shows the entire map might need some trial and error
+    latitude=0, 
+    longitude=0, 
+    zoom=1
 )
 
-# Create the Deck.gl map
+# Render the map with the custom colors
 r = pdk.Deck(
-    layers=[layer],
+    layers=[layer], 
     initial_view_state=view_state,
-    map_style=pdk.map_styles.LIGHT,  # A light map style
-    width='100%',  # Width of the map
-    height=500,  # Height of the map in pixels
+    map_style='mapbox://styles/mapbox/light-v9'
 )
 
 st.pydeck_chart(r)
-st.write ('hdfbsjhsdbfkjhb')
