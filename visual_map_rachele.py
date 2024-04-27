8121bd342bc1a2892cb8f197c077dab305f5aff9
rachelemantero <163310457+rachelemantero@users.noreply.github.com>
2024-04-27 22:42:31 +0200

    s

--- visual_map_rachele.py
+++ visual_map_rachele.py
@@ -1,23 +1,2 @@
-import streamlit as st
-import pydeck as pdk
-import geopandas as gpd
-
-# Load geographical data
-geo_data = gpd.read_file("path/to/your/global_states.geojson")
-
-# Define the Pydeck layer as a GeoJsonLayer
-layer = pdk.Layer(
-    "GeoJsonLayer",
-    geo_data,
-    opacity=0.8,
-    stroked=False,
-    filled=True,
-    extruded=True,
-    wireframe=True,
-)
-
-# Set the viewport location
-view_state = pdk.ViewState(latitude=0, longitude=0, zoom=1)
-
-# Render the map
-st.pydeck_chart(pdk.Deck(layers=[layer], initial_view_state=view_state))
+import sys
+st.write(sys.executable)
