c9908ced26de310f732a6b14b029cd224b659e9d
zeliasara <164767492+zeliasara@users.noreply.github.com>
2024-04-28 09:44:36 +0200

    Update one_state_copy.py

--- one_state_copy.py
+++ one_state_copy.py
@@ -3,6 +3,28 @@ import streamlit as st
 import requests
 from DataBase_Countries import get_country_code, get_country_name
 
+def get_country_color(country_code, visa_data):
+    if country_code in visa_data:
+        visa_status = visa_data[country_code]
+        if visa_status == 'vf':
+            return 'green'  # Visa Free
+        elif visa_status == 'voa':
+            return 'orange'  # Visa on Arrival
+        elif visa_status == 'vr':
+            return 'red'  # Visa Required
+    return 'gray'  # No data
+
+def plot_map(visa_data):
+    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+
+    world['color'] = world['iso_a3'].apply(lambda x: get_country_color(x, visa_data))
+
+    fig, ax = plt.subplots(figsize=(15, 10))
+    world.plot(ax=ax, color=world['color'])
+    ax.set_title('Visa Requirements Map')
+    ax.set_axis_off()
+    st.pyplot(fig)
+
 def run_visa_country_status():
     st.title('Visa Country Status')
 
