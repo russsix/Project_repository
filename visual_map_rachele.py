64588650a80c97039ef586359dc89368184e68cc
rachelemantero <163310457+rachelemantero@users.noreply.github.com>
2024-04-30 15:13:52 +0200

    s

--- visual_map_rachele.py
+++ visual_map_rachele.py
@@ -14,11 +14,12 @@ def fetch_visa_status_data(passport_code):
     data = response.json()
     return data
 
-visa_required_countries = [get_country_name(code) for code in data.get('vr', {}).get('data', [])]
-visa_on_arrival_countries = [get_country_name(code) for code in data.get('voa', {}).get('data', [])]
-visa_free_countries = [get_country_name(code) for code in data.get('vf', {}).get('data', [])]
+def color_for_visa_status(country, visa_data):
+    """Assign color based on visa requirement status."""
+    visa_required_countries = [get_country_name(code) for code in visa_data.get('vr', {}).get('data', [])]
+    visa_on_arrival_countries = [get_country_name(code) for code in visa_data.get('voa', {}).get('data', [])]
+    visa_free_countries = [get_country_name(code) for code in visa_data.get('vf', {}).get('data', [])]
 
-def color_for_visa_status(country):
     if country in visa_required_countries:
         return 'red'
     elif country in visa_on_arrival_countries:
@@ -32,8 +33,8 @@ def plot_map(visa_data):
     """Plot the world map with countries colored based on visa requirement status."""
     world = gpd.read_file("D:\\Download\\global_states.geojson")
 
-    # Use 'ADM0_A3' as the country code column in your GeoDataFrame
-    world['color'] = world['ADMIN'].apply(color_for_visa_status)
+    # Use 'ADMIN' as the country name column in your GeoDataFrame
+    world['color'] = world['ADMIN'].apply(lambda x: color_for_visa_status(x, visa_data))
 
     fig, ax = plt.subplots(1, figsize=(15, 10))
     world.plot(ax=ax, color=world['color'], linewidth=0.5, edgecolor='black')
@@ -50,9 +51,7 @@ def run_visa_country_status():
     if st.button('Show Visa Requirements Map'):
         data = fetch_visa_status_data(passport_code)
         if data:
-            # Prepare visa data dictionary with ISO codes as keys and visa status as values
-            visa_data = {code: status for status, codes in data.items() for code in codes}
-            plot_map(visa_data)
+            plot_map(data)
         else:
             st.error("No visa data available for the selected country.")
 
