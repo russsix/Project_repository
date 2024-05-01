923f373156c3a93eac4cad80239eee0ec7ddfc5f
rachelemantero <163310457+rachelemantero@users.noreply.github.com>
2024-05-01 14:47:18 +0200

    s

--- two_states.py
+++ two_states.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append('D:\\Download')
 from DataBase_Countries import get_country_code, country_codes
 
-def run_visa_checker():
+def run_visa_checker(prefix =""):
     st.title('Visa Requirement Checker')
 
     departure_country = st.selectbox("Select your departure country:", [""] + list(country_codes.keys()), key=f'{prefix}_two_states_departure_country')
