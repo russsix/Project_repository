7f86a6d6416201980fc63048afbcb1d043207fcc
rachelemantero <163310457+rachelemantero@users.noreply.github.com>
2024-05-01 14:37:14 +0200

    s

--- one_state.py
+++ one_state.py
@@ -86,7 +86,7 @@ def plot_map(visa_data):
 
 def run_visa_country_status():
     st.title('Visa Country Status')
-    selected_country = st.selectbox('Select your passport country:', list(country_codes.keys()))
+    selected_country = st.selectbox('Select your passport country:', list(country_codes.keys()), key = 'one_state_selected_country')
     passport_code = get_country_code(selected_country)
 
     if st.button('Show Visa Requirements Map'):
