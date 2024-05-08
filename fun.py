3832a477319be2cbe94f787b46e1625f75ec6d97
zeliasara <164767492+zeliasara@users.noreply.github.com>
2024-05-08 14:08:51 +0200

    Update fun.py

--- fun.py
+++ fun.py
@@ -8,4 +8,9 @@ from one_state import run_visa_country_status
 
 from DataBase_Countries import country_codes, get_country_code, get_country_name
 
-def get_visa_rank(
+def get_visa_rank()
+visa_free_countries = [get_country_name(code) for code in visa_data.get('vf', {}).get('data', [])]
+if visa_free_countries:
+    rank = visa_free_countries.index("Switzerland") + 1
+    st.write(f"Did you know that (country) is number {rank} with visa free states.")
+
