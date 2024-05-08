fbf6ef16482012f503ecad808535ba2add663b1c
zeliasara <164767492+zeliasara@users.noreply.github.com>
2024-05-08 12:35:42 +0200

    Update fun.py

--- fun.py
+++ fun.py
@@ -4,5 +4,8 @@ import geopandas as gpd
 import plotly.express as px
 import sys
 import pandas as pd
+from one_state import run_visa_country_status
 
 from DataBase_Countries import country_codes, get_country_code, get_country_name
+
+def get_visa_rank(
