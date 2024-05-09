9a7179a0c1cdb40fd9e9036415fc8017a02ce93f
zeliasara <164767492+zeliasara@users.noreply.github.com>
2024-05-09 22:58:12 +0200

    Update ciao.py

--- ciao.py
+++ ciao.py
@@ -2,7 +2,7 @@ import streamlit as st
 import sys
 sys.path.append('rachelemantero/Project_repository')
 from two_states import run_visa_checker
-from one_state import run_visa_country_status
+from fun import run_visa_country_status
 
 
 
