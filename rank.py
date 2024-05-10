46d8aafe009e80b18d0c02aa9c3791b7cb174640
zeliasara <164767492+zeliasara@users.noreply.github.com>
2024-05-10 19:40:43 +0200

    Update rank.py

--- rank.py
+++ rank.py
@@ -6,25 +6,11 @@ def fetch_visa_status_data(passport_code):
     response = requests.get(url)
     data = response.json()
     return data
+    
 
-
-def get_visa_free_count(passport_code):
-    data = fetch_visa_status_data(passport_code)
-    visa_free_countries = [country for country, status in data.items() if status == "visa-free"]
-    return len(visa_free_countries)
-
-
-def get_ranked_countries():
-    """Get a list of countries ranked by the number of visa-free countries."""
-    country_ranks = []
-
-    for country, code in country_codes.items():
-        visa_free_count = get_visa_free_count(code)
-        country_ranks.append((country, visa_free_count))
-
-    # Sort by visa-free count descending
-    ranked_countries = sorted(country_ranks, key=lambda x: x[1], reverse=True)
-    return ranked_countries
-
-# Execute the ranking function
-ranked_countries()
+def count_visa_free_states(visa_data):
+    result = [(country, sum(1 for visa_status in destinations.values() if visa_status == 'visa-free'))
+        for country, destinations in visa_data.items() ]
+    # Sort the result by the count of visa-free countries in descending order
+    sorted_result = sorted(result, key=lambda x: x[1], reverse=True)
+    return sorted_result
