621b56cb8a8d6db8b42dd0cf49320557600f57aa
rachelemantero <163310457+rachelemantero@users.noreply.github.com>
2024-05-10 11:37:31 +0200

    s

---
+++ rank.py
@@ -0,0 +1,7 @@
+
+def fetch_visa_status_data(passport_code):
+    """Fetch visa status data from the API."""
+    url = f'https://rough-sun-2523.fly.dev/api/{passport_code}'
+    response = requests.get(url)
+    data = response.json()
+    return data
