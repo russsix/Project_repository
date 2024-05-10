0672803e0bc79cfb424b3e10c28a913c80ae4d80
zeliasara <164767492+zeliasara@users.noreply.github.com>
2024-05-10 19:49:51 +0200

    Update rank.py

--- rank.py
+++ rank.py
@@ -7,10 +7,13 @@ def fetch_visa_status_data(passport_code):
     data = response.json()
     return data
     
+#takes the dictionary 
+def count_visa_free_countries(country_codes):
+    #empty dictionary to store visa count
+    visa_free_counts = {}
+    for country, code in country_codes.items():
+        visa_free = fetch_visa_free_countries(code)
+        visa_free_counts[country] = len(visa_free)
+    sorted_visa_free = sorted(visa_free_counts.items(), key=lambda x: x[1], reverse=True)
+    return sorted_visa_free
 
-def count_visa_free_states(visa_data):
-    result = [(country, sum(1 for visa_status in destinations.values() if visa_status == 'visa-free'))
-        for country, destinations in visa_data.items() ]
-    # Sort the result by the count of visa-free countries in descending order
-    sorted_result = sorted(result, key=lambda x: x[1], reverse=True)
-    return sorted_result
