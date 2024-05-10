d4d5bd9787e49d54df3f2721636c8076aff101f8
rachelemantero <163310457+rachelemantero@users.noreply.github.com>
2024-05-10 19:10:01 +0200

    s

--- dabataser.py
+++ dabataser.py
@@ -1,6 +1,11 @@
 import requests
-from DataBase_Countries import country_codes
 import streamlit as st
+
+# Assuming 'country_codes' is your dictionary of countries
+country_codes = {"Switzerland": "CH", "Italy": "IT", "Germany": "DE", "Afghanistan":"AF"}
+    # "CountryName": "CountryCode",
+    # Add your complete country codes dictionary here
+
 
 def fetch_visa_free_count(country_code):
     url = f'https://rough-sun-2523.fly.dev/api/{country_code}'
@@ -11,27 +16,15 @@ def fetch_visa_free_count(country_code):
             return len(data.get('vf', {}).get('data', []))
     return 0  # Return 0 if there's an error or no data
 
-def get_country_code(country_name):
-    return country_codes.get(country_name)
-
 # Dictionary to store the number of visa-free countries per code
 visa_free_counts = {}
 
 # Fetch visa-free counts for each country code
 for name, code in country_codes.items():
-    visa_free_counts[name] = fetch_visa_free_count(code)  # Changed index to name for better readability
+    visa_free_counts[code] = fetch_visa_free_count(code)
 
 # Sort countries by the number of visa-free countries in descending order
 sorted_visa_free_counts = sorted(visa_free_counts.items(), key=lambda item: item[1], reverse=True)
 
-# Displaying the sorted list in a nicer format
-st.write("Visa-Free Access Count by Country:")
-st.dataframe(sorted_visa_free_counts, width=500, height=400)  # Using a DataFrame for better UI presentation
-
-selected_country = st.selectbox('Select your passport country:', list(country_codes.keys()), key='status_selected_country')
-country_code = get_country_code(selected_country)
-
-# Optionally display details for the selected country
-if st.button('Show Visa-Free Details'):
-    visa_free_count = fetch_visa_free_count(country_code)
-    st.write(f'{selected_country} has visa-free access to {visa_free_count} countries.')
+# Print the sorted list
+st.write(sorted_visa_free_counts)
