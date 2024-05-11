568addda6e16e296aee82d481df955e7919c2e73
rachelemantero <163310457+rachelemantero@users.noreply.github.com>
2024-05-11 14:36:10 +0200

    s

--- two_states.py
+++ two_states.py
@@ -68,8 +68,12 @@ def display_visa_free_destinations(departure_code):
     if response.status_code == 200:
         data = response.json()
         visa_free_countries = [get_country_name(code) for code in data.get('vf', {}).get('data', [])]
-        st.write("Visa-free destinations:")
-        st.write(visa_free_countries)
+        if visa_free_countries:
+            # Join all country names into a single string with each name on a new line
+            formatted_countries = "\n".join(visa_free_countries)
+            st.text(formatted_countries)  # Using st.text to maintain text format without additional styling
+        else:
+            st.write("No visa-free destinations available.")
     else:
         st.error("Failed to retrieve visa-free destinations.")
 
