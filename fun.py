29e35e832e9e0e2e2c48e8e2c24340e1dfe6f5bd
zeliasara <164767492+zeliasara@users.noreply.github.com>
2024-05-11 12:10:07 +0200

    Update fun.py

--- fun.py
+++ fun.py
@@ -12,16 +12,8 @@ def fetch_and_sort_visa_data(passport_code):
     url = f'https://rough-sun-2523.fly.dev/api/{passport_code}'
     response = requests.get(url)
     data = response.json()
+    return data
     
-    # Calculating visa-free rankings within this function
-    visa_free_counts = {}
-    for country, code in country_codes.items():
-        visa_free = [get_country_name(code) for code in data.get('vf', {}).get('data', [])]
-        visa_free_counts[country] = len(visa_free)
-    sorted_visa_free = sorted(visa_free_counts.items(), key=lambda x: x[1], reverse=True)
-    
-    return data, sorted_visa_free
-
 def plot_map(data):
     """Plotting the visa requirements map."""
     # Implementation for plotting, using data
