f394e3c98af8d2e3b43f749fd238ce0333ca3cad
rachelemantero <163310457+rachelemantero@users.noreply.github.com>
2024-05-11 13:35:05 +0200

    s

--- two_states.py
+++ two_states.py
@@ -1,4 +1,3 @@
-#two_states.py
 import streamlit as st
 import requests
 
@@ -9,40 +8,45 @@ from one_state import run_visa_country_status
 def run_visa_checker():
     st.title('Visa Requirement Checker')
 
-    departure_country = st.selectbox("Select your passport country:", [""] + list(country_codes.keys()), key = 'checker_departure_country')
-    destination_country = st.selectbox("Select your destination country:", [""] + list(country_codes.keys()), key = 'checker_destination_country')
+    # Initialize session state variables if they don't exist
+    if 'show_visa_free_destinations' not in st.session_state:
+        st.session_state.show_visa_free_destinations = False
 
-    #get the names of the country codes
+    departure_country = st.selectbox("Select your passport country:", [""] + list(country_codes.keys()), key='checker_departure_country')
+    destination_country = st.selectbox("Select your destination country:", [""] + list(country_codes.keys()), key='checker_destination_country')
+
     departure_code = get_country_code(departure_country) if departure_country else None
     destination_code = get_country_code(destination_country) if destination_country else None
 
-    #print visa requirements between the two (visa-free, visa on arrival, visa required, covid-ban, no admission)
     if st.button('Check Visa Requirement'):
         url = f'https://rough-sun-2523.fly.dev/api/{departure_code}/{destination_code}'
         response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
-            if 'VF' in data.get('status', ''):
-                duration = data.get('dur', None)
-                if duration:
-                    st.success(f'A visa is not required up until {duration} days.')
-                else:
-                    st.success('A visa is not required.')
-            elif 'VOA' in data.get('status', ''):
-                st.warning ('You will obtain a visa upon arrival.')
-            elif 'VR' in data.get ('status', ''):
-                st.error ('A visa is required.')
-                st.info('Not what you were expecting? Check out our "Visa Country Status" feature to see all your visa-free destinations.')
-                if st.button("See Visa-Free Destinations"):
-                    run_visa_country_status()
-            elif 'CB' in data.get ('status', ''):
-                st.error ('Travel is currently banned due to Covid-19 restrictions.')
-            elif 'NA' in data.get ('status', ''):
-                st.error ('No entry is permitted to travelers from your country.')
-            else:
-                st.warning('The visa requirement for your destination is not clear or is unspecified.')
+            visa_status = data.get('status', '')
+            match visa_status:
+                case 'VF':
+                    duration = data.get('dur', None)
+                    message = f'A visa is not required up until {duration} days.' if duration else 'A visa is not required.'
+                    st.success(message)
+                case 'VOA':
+                    st.warning('You will obtain a visa upon arrival.')
+                case 'VR':
+                    st.error('A visa is required.')
+                    st.info('Not what you were expecting? Check out our "Visa Country Status" feature to see all your visa-free destinations.')
+                    if st.button("See Visa-Free Destinations"):
+                        st.session_state.show_visa_free_destinations = True
+                case 'CB':
+                    st.error('Travel is currently banned due to Covid-19 restrictions.')
+                case 'NA':
+                    st.error('No entry is permitted to travelers from your country.')
+                case _:
+                    st.warning('The visa requirement for your destination is not clear or is unspecified.')
         else:
             st.error(f"Failed to retrieve data. Status code: {response.status_code}")
+
+    if st.session_state.show_visa_free_destinations:
+        run_visa_country_status()
 
 if __name__ == "__main__":
     run_visa_checker()
