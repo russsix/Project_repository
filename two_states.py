75dd9e6bbf35fb8159212e2a69fe1653d538d2ef
rachelemantero <163310457+rachelemantero@users.noreply.github.com>
2024-05-11 14:40:21 +0200

    s

--- two_states.py
+++ two_states.py
@@ -53,8 +53,8 @@ def handle_visa_result():
         st.warning('You will obtain a visa upon arrival.')
     elif visa_status == 'VR':
         st.error('A visa is required.')
+        st.write ('Is this not what you were expecting? To see al your visa-free destinations click the button below.')
         if st.button("See Visa-Free Destinations"):
-            st.write ('Is this not what you were expecting? To see al your visa-free destinations click the button below.')
             st.session_state.current_page = 'visa_free_destinations'
     elif visa_status == 'CB':
         st.error('Travel is currently banned due to Covid-19 restrictions.')
