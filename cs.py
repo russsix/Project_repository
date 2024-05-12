471875a2b8a3ebfde6fb7c91add232dcd10cc0c4
rachelemantero <163310457+rachelemantero@users.noreply.github.com>
2024-05-12 12:52:52 +0200

    s

--- cs.py
+++ cs.py
@@ -1,27 +1,26 @@
-#cs.py
 import streamlit as st
 import sys
 sys.path.append('rachelemantero/Project_repository')
 from two_states import run_visa_checker
 from one_state import run_visa_country_status
 
-
-
 def main():
-    st.title("Check-It ✅")
-    
-    # Sidebar title
-    st.sidebar.title('Navigation')
+    # Set the page title without a favicon
+    st.set_page_config(page_title="Check-It")
+
+    # Using HTML to style the sidebar title as smaller and aligned at the top-left
+    st.sidebar.markdown("<h1 style='font-size: 18px; color: black; margin-top: 0;'>Check-It ✅</h1>", unsafe_allow_html=True)
+
+    # Sidebar navigation and welcome text
     st.sidebar.write("Welcome to the Visa and Travel Assistant! Select a feature from the list below to get started.")
     app_option = st.sidebar.selectbox('Choose a feature:',
-        ('Two States', 'Visa Country Status', 'Flight Comparison'))
+                                      ['Two States', 'Visa Country Status', 'Flight Comparison'])
 
+    # Execute appropriate function based on user choice
     if app_option == 'Two States':
         run_visa_checker()
     elif app_option == 'Visa Country Status':
         run_visa_country_status()
-
-        
 
 if __name__ == "__main__":
     main()
